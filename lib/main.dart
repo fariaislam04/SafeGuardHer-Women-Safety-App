83e3994ffff998cfb1d689f25561807d8a876ceb
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-06-16 19:37:33 +0600

    feat: firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:safeguardher_flutter_app/screens/home_screen/home_screen.dart';
+import 'firebase_options.dart';
 
-void main () {
+Future<void> main () async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(const SafeGuardHer());
