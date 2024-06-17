7a8dca29d6419b9fc137b9a8b90a6c1c15c362a3
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-06-18 01:33:30 +0600

    feat: stop alert screen

---
+++ lib/screens/panic_button_screen/stop_panic_alert_screen/stop_panic_alert_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:safeguardher_flutter_app/screens/home_screen/home_screen.dart';
+
+/*
+    This page shows up when the user presses "STOP SOS ALERT" from panic
+    screens. This page shows the user that panic alert has successfully
+    stopped, and returns to the home page after a delay of 2 seconds.
+ */
+
+class StopPanicAlertScreen extends StatelessWidget {
+  const StopPanicAlertScreen({super.key});
+
+  Future<void> navigateToHome(context) async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+        const HomeScreen(),
+        transitionDuration: const Duration(milliseconds: 500),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SchedulerBinding.instance.addPostFrameCallback((_) => navigateToHome(context));
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 30.0),
+                Container(
+                  width: 300.0,
+                  height: 300.0,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/warning.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40.0),
+                const Text(
+                  'Emergency SOS Alert Stopped!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFF263238),
+                    fontSize: 28,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    height: 1.2,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  'You will return to the home screen within a few seconds.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFF263238),
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
