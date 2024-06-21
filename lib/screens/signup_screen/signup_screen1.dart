86ab005bd126a8c8d3949947d3ce5e96d0fd4aea
fariaislam04 <mytul1242@gmail.com>
2024-06-21 20:24:21 +0600

    feat: add sign up screen 1 and 2

---
+++ lib/screens/signup_screen/signup_screen1.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:safeguardher_flutter_app/screens/login_screen/login_screen.dart';
+import 'package:safeguardher_flutter_app/screens/signup_screen/signup_screen2.dart';
+
+class SignUpScreen1 extends StatefulWidget {
+  const SignUpScreen1({super.key});
+
+  @override
+  _SignUpScreen1State createState() => _SignUpScreen1State();
+}
+
+class _SignUpScreen1State extends State<SignUpScreen1> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  String? _selectedGender;
+  IconData _genderIcon = Icons.female; // Default icon
+
+  void _updateGenderField(String? newValue) {
+    setState(() {
+      _genderController.text = newValue ?? '';
+      _isGenderField = _selectedGender != null;
+      _selectedGender = newValue;
+      // Update icon based on selected gender
+      if (newValue == 'Male') {
+        _genderIcon = Icons.male;
+      } else if (newValue == 'Female') {
+        _genderIcon = Icons.female;
+      } else {
+        _genderIcon = Icons.transgender;
+      }
+    });
+  }
+
+  bool _isUsernameField = false;
+  bool _isDobField = false;
+  bool _isGenderField = false;
+
+  bool get _isButtonEnabled =>
+      _isUsernameField && _isDobField && _isGenderField;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _dobController.text = "${picked.toLocal()}".split(' ')[0];
+        _isDobField = _dobController.text.isNotEmpty;
+      });
+    }
+  }
+
+  void _updateUsernameField() {
+    setState(() {
+      _isUsernameField = _usernameController.text.isNotEmpty;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController.addListener(_updateUsernameField);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 0),
+              SvgPicture.asset(
+                'assets/logos/logo.svg',
+                height: 60,
+              ),
+              const SizedBox(height: 30),
+              SvgPicture.asset(
+                'assets/illustrations/login1.svg',
+                height: 45,
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'User Information',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Please fill up some basic information and we can set up your account on SafeGuardHer!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Username                                                                         ',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  hintText:
+                      'Enter your username', // Placeholder text inside the box
+                  hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
+                  prefixIcon: const Icon(Icons.person),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Date of Birth                                                                      ',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _dobController,
+                decoration: InputDecoration(
+                  hintText:
+                      'Enter your date of birth', // Placeholder text inside the box
+                  hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
+                  prefixIcon: const Icon(Icons.calendar_month_outlined),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                onTap: () => _selectDate(context),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Gender                                                                                 ',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 8),
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  hintText: 'Select your gender',
+                  hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
+                  prefixIcon: Icon(_genderIcon),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                value: _selectedGender,
+                style: const TextStyle(
+                    color: Colors.black, fontSize: 16, fontFamily: 'Poppins'),
+                items: <String>['Male', 'Female', 'Others'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Poppins'),
+                    ),
+                  );
+                }).toList(),
+                onChanged: _updateGenderField,
+              ),
+              const SizedBox(height: 35),
+              ElevatedButton(
+                onPressed: _isButtonEnabled
+                    ? () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpScreen2()),
+                        );
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      _isButtonEnabled ? const Color(0xFFD20451) : Colors.grey,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 143, vertical: 14),
+                ),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
