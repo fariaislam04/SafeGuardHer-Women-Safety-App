5e1d9ca7ca3df04171b89f0a99ed0d95754a8f3b
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-06-24 01:27:16 +0600

    feat: safety tips page

---
+++ lib/screens/safety_tips_screen/safety_at_work_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SafetyAtWorkPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Safety at Work'),
+      ),
+      body: Center(
+        child: Text('Content for Safety at Work'),
+      ),
+    );
+  }
+}
