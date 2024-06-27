1c7d456f1c29fba8d3fa4736566f47bc67e22a8a
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-06-27 23:13:32 +0600

    feat: report incident screen with snackbar

--- lib/screens/report_incident_screen/report_incident_screen.dart
+++ lib/screens/report_incident_screen/report_incident_screen.dart
@@ -9,35 +9,50 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: ReportIncidentPage(),
     );
   }
 }
 
 class ReportIncidentPage extends StatefulWidget {
+  const ReportIncidentPage({super.key});
+
   @override
-  _ReportIncidentPageState createState() => _ReportIncidentPageState();
+  ReportIncidentPageState createState() => ReportIncidentPageState();
 }
 
-class _ReportIncidentPageState extends State<ReportIncidentPage> {
-  String dropdownValue = 'Harassment'; // Initial value for the dropdown
+class ReportIncidentPageState extends State<ReportIncidentPage> {
+  String dropdownValue = 'Harassment';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF831D2D),
+      backgroundColor: const Color(0xFF831D2D),
+      appBar: AppBar(
+        toolbarHeight: 90,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: SvgPicture.asset(
+            'assets/logos/logo_dark_theme.svg',
+            height: 70,
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
               Container(
-                padding: EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(30.0),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
+                  color: const Color(0XFFF5F5F5),
+                  borderRadius: BorderRadius.circular(30.0),
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -45,35 +60,39 @@ class _ReportIncidentPageState extends State<ReportIncidentPage> {
                     const Text(
                       'Report Incident',
                       style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
                         color: Colors.black,
+                        fontFamily: 'Poppins',
                       ),
                     ),
-                    SizedBox(height: 16.0),
+                    const SizedBox(height: 16.0),
                     const Text(
                       'Kindly take a few minutes to report the incident you faced. It will help us mark unsafe places for other users.',
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 11,
                         color: Colors.black54,
+                        fontFamily: 'Poppins',
                       ),
                     ),
-                    SizedBox(height: 8.0),
+                    const SizedBox(height: 10.0),
                     const Text(
                       'You can also submit later from history.',
                       style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                        fontWeight: FontWeight.w600,
                         color: Colors.black,
+                        fontFamily: 'Poppins',
                       ),
                     ),
-                    SizedBox(height: 24.0),
+                    const SizedBox(height: 24.0),
                     const Text(
                       'Type of Incident',
                       style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
                         color: Colors.black,
+                        fontFamily: 'Poppins',
                       ),
                     ),
                     const SizedBox(height: 8.0),
@@ -84,73 +103,152 @@ class _ReportIncidentPageState extends State<ReportIncidentPage> {
                           dropdownValue = newValue!;
                         });
                       },
-                      items: <String>['Harassment', 'Assault', 'Theft', 'Other']
-                          .map<DropdownMenuItem<String>>((String value) {
+                      items: <String>[
+                        'Harassment',
+                        'Assault',
+                        'Theft',
+                        'Robbery',
+                        'Stalking',
+                        'Violence',
+                        'Domestic Violence',
+                        'Other'
+                      ].map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
-                          child: Text(value),
+                          child: Text(
+                            value,
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         );
                       }).toList(),
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF831D2D),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
                         ),
                         filled: true,
-                        fillColor: Colors.grey[200],
+                        fillColor: Colors.white,
+                        focusColor: const Color(0xFF831D2D),
                       ),
                     ),
-                    SizedBox(height: 24.0),
+                    const SizedBox(height: 24.0),
                     const Text(
                       'Additional Remarks',
                       style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
                         color: Colors.black,
+                        fontFamily: 'Poppins',
                       ),
                     ),
-                    SizedBox(height: 8.0),
+                    const SizedBox(height: 8.0),
                     TextField(
                       maxLines: 5,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF831D2D),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
                         ),
                         filled: true,
-                        fillColor: Colors.grey[200],
+                        fillColor: Colors.white,
                       ),
                     ),
                     const SizedBox(height: 24.0),
-                    Center(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF831D2D),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 32.0, vertical: 16.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
+                    LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Center(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF831D2D),
+                              padding: const EdgeInsets.symmetric(vertical:
+                              14.0),
+                              minimumSize: Size(constraints.maxWidth, 40),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                            onPressed: ()
+                            {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: const Row(
+                                    children: [
+                                      Icon(Icons.check_circle, color: Colors.white),
+                                      SizedBox(width: 10),
+                                      Text('Your report has been submitted!'),
+                                    ],
+                                  ),
+                                  backgroundColor: Colors.green,
+                                  behavior: SnackBarBehavior.floating,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  duration: const Duration(seconds: 1),
+                                ),
+                              );
+                              Future.delayed(const Duration(seconds: 2), ()
+                              {
+                                Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(builder: (context) => const HomeScreen()),
+                                (Route<dynamic> route) => false,
+                                );
+                              });
+                            },
+                            child: const Text(
+                              'Submit',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
-                        ),
-                        onPressed: () {
-                          // Handle Submit button press
-                        },
-                        child: const Text('Submit', style: TextStyle(fontFamily: 'P'
-                            'oppins', color: Colors.white)),
-                      ),
+                        );
+                      },
                     ),
+                    const SizedBox(height: 15.0),
                     Center(
                       child: TextButton(
                         onPressed: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => const HomeScreen
-                              ()),
+                            MaterialPageRoute(builder: (context) => const HomeScreen()),
                           );
                         },
                         child: const Text(
                           'Submit Later',
                           style: TextStyle(
-                            fontSize: 16,
+                            fontSize: 13,
                             color: Color(0xFF831D2D),
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline,
                           ),
                         ),
                       ),
