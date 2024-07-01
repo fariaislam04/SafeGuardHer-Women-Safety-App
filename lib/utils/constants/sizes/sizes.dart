db5d1cca2b1048a083de7af4f8ca297a1fed2299
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-07-02 01:08:23 +0600

    fix: fix inconsistent font sizes

--- lib/utils/constants/sizes/sizes.dart
+++ lib/utils/constants/sizes/sizes.dart
@@ -1,15 +1,16 @@
-class FontSizes
+class Sizes
 {
-  FontSizes._();
+  Sizes._();
 
   //Font sizes
-  static const double fontHeading = 16.0;
-  static const double fontSubHeading = 14.0;
-  static const double fontSubtitle = 11.0;
+  static const double fontHeading = 14.0;
+  static const double fontSubHeading = 12.0;
+  static const double fontSubtitle = 10.0;
+  static const double fontLarge = 20.0;
 
   //Icon sizes
   static const double iconLarge = 35;
-  static const double iconMedium = 35;
+  static const double iconMedium = 30;
   static const double iconSmall = 24;
 
 }
