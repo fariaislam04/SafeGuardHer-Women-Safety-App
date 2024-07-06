7f1a6232877cdd3a4e0c5200f4c75678c55a9556
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-07-06 14:05:58 +0600

    feat: helper functions
    Contains extractTodayDate(), extractTodayTime(), goTo(), and appLoader()

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -1,40 +1,72 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:safeguardher_flutter_app/utils/constants/colors/colors.dart';
 
-class GenerateSafeCodeForRescue
+class AppHelperFunctions
 {
+  static String extractTodayDate()
+  {
+    return DateTime.now().toString().split(' ')[0];
+  }
+
+  static String extractTodayTime()
+  {
+  return DateTime.now().toString().split(' ')[1].split('.')[0];
+  }
+
   static int generateSafeCodeForRescue()
   {
-    Random random = Random();
-    int code = 1000 + random.nextInt(9000);
+    final random = Random();
+    final code = 1000 + random.nextInt(9000);
     return code;
   }
-}
 
-void goTo(BuildContext context, Widget nextScreen)
-{
-  Navigator.push(
+  void goTo(BuildContext context, Widget nextScreen)
+  {
+    Navigator.pushReplacement(
       context,
-      MaterialPageRoute(
-        builder: (context) => nextScreen,
-      ));
-}
+      MaterialPageRoute(builder: (context) => nextScreen),
+    );
+  }
 
-dialogueBox(BuildContext context, String text) {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text(text),
-    ),
-  );
-}
+  void goBack(BuildContext context)
+  {
+    Navigator.pop(
+      context
+    );
+  }
 
-Widget progressIndicator(BuildContext context)
-{
-  return const Center(
-      child: CircularProgressIndicator(
-        color: Colors.red,
-        strokeWidth: 7,
-      ));
-}
+  static void showAlert(BuildContext context, String title, String message)
+  {
+    showDialog(
+        context: context,
+        builder: (BuildContext context)
+        {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Close')
+              )
+            ],
+          );
+        }
+    );
+  }
 
+  Widget appLoader(BuildContext context)
+  {
+    return Scaffold(
+      body: Center(
+        child: LoadingAnimationWidget.flickr(
+          leftDotColor: AppColors.logoPrimary,
+          rightDotColor: AppColors.logoSecondary,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
