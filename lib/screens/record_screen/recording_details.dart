e7e9c2e94aa93438688ab948bec0b3af29122f28
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-07-06 21:06:40 +0600

    feat: fetch images from storage, format the dates and time, and show in PhotoWidget

--- lib/screens/record_screen/recording_details.dart
+++ lib/screens/record_screen/recording_details.dart
@@ -1,133 +1,181 @@
 import 'package:flutter/material.dart';
 import 'package:voice_message_package/voice_message_package.dart';
 import 'package:photo_view/photo_view.dart';
+import '../../services/background/storage_service/storage_service.dart';
+import '../../utils/constants/colors/colors.dart';
+import '../../utils/formatters/formatters.dart';
 import '../../utils/helpers/helper_functions.dart';
 import '../../widgets/navigations/app_bar.dart';
+
 AppHelperFunctions appHelperFunctions = AppHelperFunctions();
 
 class RecordingDetails extends StatelessWidget {
   final String audioUrl = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  final String uid;
+  final String date;
 
-  const RecordingDetails({super.key});
+  const RecordingDetails({super.key, required this.uid, required this.date});
 
   @override
   Widget build(BuildContext context) {
+    String formattedDate = Formatters.formatDateString(date);
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: const CustomAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 15.0, top: 3.0, right: 15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back_ios,
-                    color: Color(0xFF263238),
-                    size: 15,
-                  ),
-                  onPressed: () => appHelperFunctions.goBack(context)
-                ),
-                const Text(
-                  'Record History of 12/05/24',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
+    return FutureBuilder<List<ImageData>>(
+      future: StorageService().listImagesForEachDate(uid, date),
+      builder: (context, snapshot)
+      {
+        // -- while app is fetching data from storage, show app loader
+        if (snapshot.connectionState == ConnectionState.waiting)
+        {
+          return Scaffold(
+            appBar: const CustomAppBar(),
+            body: Center(
+              child: appHelperFunctions.appLoader(context),
             ),
-            const SizedBox(height: 0.0),
-            const Text(
-              'Record lasted 30 minutes.',
-              style: TextStyle(
-                fontSize: 11,
-                fontFamily: 'Poppins',
-                color: Colors.grey,
-              ),
+          );
+        }
+
+        // -- If there's an error, show error
+        else if (snapshot.hasError)
+        {
+          return Scaffold(
+            appBar: const CustomAppBar(),
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
             ),
-            const Divider(color: Color(0xFFEDEDED)),
-            const SizedBox(height: 10),
-            const Text(
-              'Photos (4)',
-              style: TextStyle(
-                  fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'Poppins'),
+          );
+        }
+
+        // -- If the date folder contains no images, show "No images"
+        else if (!snapshot.hasData || snapshot.data!.isEmpty)
+        {
+          return const Scaffold(
+            appBar: CustomAppBar(),
+            body: Center(
+              child: Text('No images found for this date.'),
             ),
-            const SizedBox(height: 8),
-            const SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+          );
+        }
+
+        // -- Show the images in PhotoWidget
+        else
+        {
+          List<ImageData> imageDatas = snapshot.data!;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: const CustomAppBar(),
+            body: Padding(
+              padding: const EdgeInsets.only(left: 15.0, top: 3.0, right: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  PhotoWidget(src: 'assets/placeholders/cam1 (1).png', time: '12:10 AM'),
-                  PhotoWidget(src: 'assets/placeholders/cam1 (2).png', time: '12:40 AM'),
-                  PhotoWidget(src: 'assets/placeholders/cam1 (3).png', time: '1:00 AM'),
-                  PhotoWidget(src: 'assets/placeholders/cam1 (4).png', time: '1:20 AM'),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back_ios,
+                          color: Color(0xFF263238),
+                          size: 15,
+                        ),
+                        onPressed: () => appHelperFunctions.goBack(context),
+                      ),
+                      Text(
+                        'Record History of $formattedDate',
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 0.0),
+                  const Text(
+                    'Record lasted 30 minutes.',
+                    style: TextStyle(
+                      fontSize: 11,
+                      fontFamily: 'Poppins',
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const Divider(color: AppColors.dividerPrimary),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Photos (${imageDatas.length})',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: imageDatas.map((data) => PhotoWidget(data: data)).toList(),
+                    ),
+                  ),
+                  const Divider(color: Color(0xFFEDEDED)),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Audios (1)',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 15),
+                  SizedBox(
+                    width: 350,
+                    child: VoiceMessageView(
+                      controller: VoiceController(
+                        audioSrc: audioUrl,
+                        onComplete: () {},
+                        onPause: () {},
+                        onPlaying: () {},
+                        onError: (err) {},
+                        maxDuration: const Duration(minutes: 30),
+                        isFile: false,
+                        noiseCount: 50,
+                      ),
+                      size: 50.0,
+                      innerPadding: 3,
+                      playIcon: const Icon(
+                        Icons.play_arrow_rounded,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      pauseIcon: const Icon(
+                        Icons.pause_rounded,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      activeSliderColor: AppColors.secondary,
+                      circlesTextStyle: const TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                      circlesColor: AppColors.secondary,
+                    ),
+                  ),
                 ],
               ),
             ),
-            const Divider(color: Color(0xFFEDEDED)),
-            const SizedBox(height: 16),
-            const Text(
-              'Audios (1)',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 15),
-            SizedBox(
-              width: 350,
-              child: VoiceMessageView(
-                controller: VoiceController(
-                  audioSrc: audioUrl,
-                  onComplete: () {},
-                  onPause: () {},
-                  onPlaying: () {},
-                  onError: (err) {},
-                  maxDuration: const Duration(minutes: 30),
-                  isFile: false,
-                  noiseCount: 50,
-                ),
-                size: 50.0,
-                innerPadding: 3,
-                playIcon: const Icon(
-                  Icons.play_arrow_rounded,
-                  size: 30.0,
-                  color: Colors.white,
-                ),
-                pauseIcon: const Icon(
-                  Icons.pause_rounded,
-                  size: 30.0,
-                  color: Colors.white,
-                ),
-                activeSliderColor: const Color(0xFFD20452),
-                circlesTextStyle: const TextStyle(
-                  fontSize: 11,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-                circlesColor: const Color(0xFFD20452),
-              ),
-            ),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
 
 class PhotoWidget extends StatelessWidget {
-  final String src;
-  final String time;
+  final ImageData data;
 
-  const PhotoWidget({super.key, required this.src, required this.time});
+  const PhotoWidget({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        appHelperFunctions.goTo(context, ZoomableImagePage(imageSrc: src));
+        appHelperFunctions.goTo(context, ZoomableImagePage(imageSrc: data.imageUrl));
       },
       child: Column(
         children: [
@@ -138,14 +186,14 @@ class PhotoWidget extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
               image: DecorationImage(
-                image: AssetImage(src),
+                image: NetworkImage(data.imageUrl),
                 fit: BoxFit.cover,
               ),
             ),
           ),
           const SizedBox(height: 4),
           Text(
-            time,
+            data.date,
             style: const TextStyle(fontFamily: 'Poppins', fontSize: 10),
           ),
         ],
@@ -167,7 +215,7 @@ class ZoomableImagePage extends StatelessWidget {
       ),
       body: Center(
         child: PhotoView(
-          imageProvider: AssetImage(imageSrc),
+          imageProvider: NetworkImage(imageSrc),
           backgroundDecoration: BoxDecoration(
             color: Theme.of(context).canvasColor,
           ),
